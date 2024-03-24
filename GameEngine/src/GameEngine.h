31f8f79c527a54f3767783b3e1b99764e9db71ee
Majd Bitar <majd.r.bitar1@gmail.com>
2024-03-24 10:17:14 +0100

    Setup basic Application and Entry Point

---
+++ GameEngine/src/GameEngine.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// For use by GameEngine applications
+// we include everything that we need in the GameEngine.h file
+// and then include this file in the application so that we don't have to include everything separately
+#include <stdio.h>
+#include "GameEngine/Application.h"
+
+// --------- Entry Point ---------
+
+#include "GameEngine/EntryPoint.h"
